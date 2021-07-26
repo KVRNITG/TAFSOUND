571074225743bbe18620e9bce83bae52d25cf5d4
Venky <kvrvenkateshkvr@gmail.com>
2021-07-26 15:08:51 +0530

    Update PF.m

--- PF.m
+++ PF.m
@@ -2,37 +2,38 @@
  clear all;
 tic;
 
-%input
+
 % ues data
 % uex = x coordinate of the ue
 % uey = y coordinate of the ue
 % sbx = x coordinate of the s-BS
 % sby = y coordinate of the s-BS
- sinrt=-5;   % predefind SINR threshold value
-% tp= transmission power of
+% sinrt;   % predefind SINR threshold value
+% tp= transmission power of UE
+%alpha=attenuation factor
+%delta= thermal noise of the UE
 % smax= mxmimum number ues supported by bs 
-smax=30;
+%pmax= Maximum transmission power of the s-BS
 %pa= Circuit power of active s-BS
 %ps= Circuit power of sleep s-BS
-
-ues=xlsread('G:\kvr\lockdown\oct-2020 onwards papers\wpc per for conferne updated one\code\WPCPoints','CUES');
+ %TR= Transmission range of s-bs
+%ues= Read the coordinates of the UEs 
+%SEE= small cell energy efficiency
 uex=ues(:,1);  %intailzation points
- uey=ues(:,2);
- %range=250;
+ uey=ues(:,2); 
 sue=size(ues,1);
- z=1;
-sbs=xlsread('G:\kvr\lockdown\oct-2020 onwards papers\wpc per for conferne updated one\code\WPCPoints','CBS');
+%sbs=Read the coordinates of the small base station
 ssb=size(sbs,1);
  sbsx=sbs(:,1);
  sbsy=sbs(:,2);
  
  
 
- yes=1;
+ Depysbs=1;
  srtpm=0;
  uel=0;
- while(yes==1)
-     [ssb,bsleast,sbsx,sbsy,Alluesconn,uelr,yes]=connb(uex,uey,sbsx,sbsy,ssb,sue,srtpm,uel,z);
+ while(Depysbs==1)
+     [ssb,bsleast,sbsx,sbsy,Alluesconn,uelr,Depysbs]=connb(uex,uey,sbsx,sbsy,ssb,sue,srtpm,uel); % establish connected network 
    
      srtpk=bsleast;
      if(Alluesconn==1)
@@ -40,13 +41,11 @@ ssb=size(sbs,1);
      else
         uel=uelr;
      end
-     [ssb,xss,yss]=rbase(bsleast,sbsx,sbsy,ssb);
+     [ssb,xss,yss]=rbase(bsleast,sbsx,sbsy,ssb); % sleep the small base station (s-BS)
 sbsx=0;sbsy=0;
-     sbsx=xss(:);
+sbsx=xss(:);
 sbsy=yss(:);
 
-z=z+1;
-     
  end
   toc;
  function [ssb,xss,yss]=rbase(bsleast,sbsx,sbsy,ssb)
@@ -64,9 +63,8 @@ q=0;
  end
  ssb=ssb-1;
 end
- function [ssb,bsleast,sbsx,sbsy,Alluesconn,uelr,yes]=connb(uex,uey,sbsx,sbsy,ssb,sue,srtpm,uel,z)
-sinrt=-5;
-smax=30; 
+ function [ssb,bsleast,sbsx,sbsy,Alluesconn,uelr,yes]=connb(uex,uey,sbsx,sbsy,ssb,sue,srtpm,uel)
+
 for i=1:ssb
     for j=1:sue        
         yDiff=sbsy(i)-uey(j);
@@ -74,7 +72,7 @@ for i=1:ssb
         xDiff=sbsx(i)-uex(j);
         xSqr=xDiff*xDiff;
         output(i,j)=sqrt(ySqr + xSqr);
-        snr(i,j)=(((1-30)*(10*log10(output(i,j)^-1.5)-30))/(-97-30)); %SINR caculation    
+        snr(i,j)=((tp*((output(i,j)^-alpha))/delta); %SINR caculation    
         
     end
 
@@ -166,7 +164,7 @@ end
 end
 end
 for i=1:ssb    
-    bsloads(i)=bsdegree(i)/smax;     % calculate the each bs
+    bsloads(i)=bsdegree(i)/smax;     % calculate the each s-BS load
     if(bsloads(i)>0)
         bsactive(i)=1;
     else
@@ -186,15 +184,15 @@ for i=1:ssb
         xDiff=sbsx(bsleast)-sbsx(i);
         xSqr=xDiff*xDiff;
         bstbsd(i)=sqrt(ySqr + xSqr);
-        if (bstbsd(i)<0.250& bstbsd(i)~=0)
+        if (bstbsd(i)<TR& bstbsd(i)~=0)
             count=count+1;
-            nblist(1,count)=i;
+            nblist(1,count)=i; % 
             nblist(2,count)=bsloads(i);
             
         end
 end
 try
-    yes=1;
+    Depysbs=1;
 [temp,order]=sort(nblist(2,:));
 snlist=nblist(:,order);
 [temp,order]=sort(nblist(2,:),'descend');
@@ -246,7 +244,7 @@ for i=1:size(snloadsm,2)
     k=0;
   
       [a,b]=sort(bsleastcuesnr(i,:),'descend')
-    while(snloadsm(4,i)~=1)
+    while(snloadsm(4,i)~=1) % share the load of least loaded s-BS to neighbour s-BS based on the percentage
         
     if(bsdegree(snloadsm(1,i)<=smax))
       for j=1:size(bsleastcue,2)
@@ -273,9 +271,9 @@ bsleshred=1;
 bsactive(bsleast)=0;
 
 %%EE
-pa=39-30;%
- ps=35-30;%convert dbm to db 35-30
-sm=30;
+%pa=power consumption of active s-BS;%
+% ps=power consumption of active s-BS;
+
 SNT=0;
 NC=0;
 ussb=ssb-1;
@@ -297,48 +295,17 @@ for c=1:ssb
     T_s(c)=(R_ss(c))*(1);
     SNT=T_s(c)+SNT;
    
-NC=NC+(bsactive(c)*((((40-30)*F_c)/sm)+pa))+((1-bsactive(c))*ps);    
+NC=NC+(bsactive(c)*(((pmax*F_c)/sm)+pa))+((1-bsactive(c))*ps);    
 
     if(c==ssb)
             SNC=NC;
         end
 end
-
-
 SEE=(SNT/SNC);
-SNT
-abs(SNT)
-
-(SEE+30)
-(SEE+30)*(1.26*10^-3);
-
-figure(z);
-
- plot(uex,uey,'bo','MarkerSize',5,'LineWidth',1,'MarkerFaceColor',[0.5,0.5,0.5]); 
- hold on;   
- grid on;
- plot(sbsx,sbsy,'r^','MarkerSize',7,'LineWidth',1,'MarkerFaceColor','r');
- A=xlsread('G:\kvr\lockdown\oct-2020 onwards papers\wpc per for conferne updated one\code\WPCPoints','CBS');
- B=[sbsx,sbsy];
- [finalsx,finalsy]=ismember(A,B,'rows'); 
- A(finalsx, :)=[];
- plot(A(:,1),A(:,2),'g^','MarkerSize',7,'LineWidth',1,'MarkerFaceColor','k');
-hold on
-xlabel('Distance');
-ylabel('Distance');
-legend('UE','s-BS');
-for i=1:ssb
-for j=1:sue
-if (Assign(2,j)==i)  
- line([uex(j),sbsx(i)], [uey(j), sbsy(i)], 'Color', 'r');   
-  hold on;   
-end
-
-end
-end
+NoofAsbs=size(sbsx,2);
 
 catch exception
-    yes=0;
+    Depysbs=0;
 break;
 end
 
